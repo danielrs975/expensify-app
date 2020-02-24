82582351523ea32ba0f60bebb645a1042649af28
Daniel <danielrs9705@gmail.com>
2020-02-25 00:20:25 +0100

    Store user data in user area

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -8,10 +8,11 @@ export const addExpense = (expense) => ({
 });
 
 export const startAddExpense = (expenseData = {}) => {
-	return (dispatch) => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
 		const { description = '', note = '', amount = 0, createdAt = 0 } = expenseData;
 		const expense = { description, note, amount, createdAt };
-		return database.ref('expenses').push(expense).then((ref) => {
+		return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => {
 			dispatch(addExpense({ id: ref.key, ...expense }));
 		});
 	};
@@ -24,8 +25,9 @@ export const removeExpense = ({ id }) => ({
 });
 
 export const startRemoveExpense = ({ id }) => {
-	return (dispatch) => {
-		return database.ref(`expenses/${id}`).remove().then(() => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
+		return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => {
 			dispatch(removeExpense({ id }));
 		});
 	};
@@ -39,8 +41,9 @@ export const editExpense = (id, updates) => ({
 });
 
 export const startEditExpense = (id, updates) => {
-	return (dispatch) => {
-		return database.ref(`expenses/${id}`).update(updates).then(() => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
+		return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
 			dispatch(editExpense(id, updates));
 		});
 	};
@@ -53,8 +56,9 @@ export const setExpenses = (expenses) => ({
 });
 
 export const startSetExpenses = () => {
-	return (dispatch) => {
-		return database.ref('expenses').once('value').then((snapshot) => {
+	return (dispatch, getState) => {
+		const uid = getState().auth.uid;
+		return database.ref(`users/${uid}/expenses`).once('value').then((snapshot) => {
 			const expenses = [];
 			snapshot.forEach((childSnapshot) => {
 				expenses.push({
--- src/components/HelpPage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const HelpPage = () => <div>This is is from my help component</div>;
-
-export default HelpPage;
